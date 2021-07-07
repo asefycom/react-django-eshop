8a62b9919728790de5967421205f57e7a23a8e4f
mostafa-asefy <asefi.v@gmail.com>
2021-07-07 17:30:08 +0430

    lession 6: Style Components using Bootstrap

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 function Footer() {
     return (
-        <div>
-            <footer>This is footer</footer>
-        </div>
+        <footer>
+            <Container>
+                <Row>
+                    <Col className="text-center py-3">Copyright &copy; E-Shop</Col>
+                </Row>
+            </Container>
+        </footer>
     )
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Navbar, Nav, Container, Row } from 'react-bootstrap'
 
 function Header() {
     return (
-        <div>
-            <header>This is header</header>
-        </div>
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">E-Shop</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                            <Nav.Link href="/login"><i className="fas fa-user"></i>Login</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     )
 }
 

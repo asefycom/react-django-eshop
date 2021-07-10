96605b9edcb1d5772614f3895d9b2f8e4e822cba
mostafa-asefy <asefi.v@gmail.com>
2021-07-10 18:59:59 +0430

    lession 7: sample data

---
+++ frontend/src/products.js
@@ -0,0 +1,83 @@
+const products = [
+  {
+    '_id': '1',
+    'name': 'ASUS TUF Dash 15 Ultra Slim Gaming Laptop',
+    'image': '/images/laptop-asus-tuf15.jpg',
+    'description':
+      'The all-new TUF Dash F15 puts powerful Windows 10 gaming in a super slim chassis. The latest 11th Gen Intel Core CPU and GeForce RTX GPU make gaming smooth on up to a blazing fast high refresh-rate display. Always ready for action, the Dash F15 darts with ease between gaming, streaming, and more.',
+    'brand': 'ASUS',
+    'category': 'Electronics',
+    'price': 946.99,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 91,
+  },
+  {
+    '_id': '2',
+    'name': 'Xbox Series X 11 Pro 256GB Memory',
+    'image': '/images/xbox-x11.jpg',
+    'description':
+      'Introducing Xbox Series X, the fastest, most powerful Xbox ever. Play thousands of titles from four Generations of Consoles - all games look and play best on Xbox Series X. At the heart of Series X is the Xbox Velocity architecture, which pairs a custom SSD with integrated software for faster, streamlined gameplay with significantly reduced load times. Seamlessly move between multiple games in a Flash with quick resume. Explore rich new worlds and enjoy the action like never before with the unmatched 12 Teraflops of raw graphic processing power. Enjoy 4K gaming at up to 120 frames per second, advanced 3D spatial sound, and more. Get started with an instant library of 100+ high-quality games, including all new Xbox Game Studios titles the day they launch like Halo Infinite, with Xbox Game Pass ultimate (membership sold separately). If you purchased Xbox All Access at Amazon, please visit the Xbox website directly for steps on how to upgrade at a participating Xbox All Access retailer. Amazon is not an Xbox All Access participating retailer for Xbox Series X|S.',
+    'brand': 'Micrisoft',
+    'category': 'Electronics',
+    'price': 708.88,
+    'countInStock': 7,
+    'rating': 4.9,
+    'numReviews': 8245,
+  },
+  {
+    '_id': '3',
+    'name': 'RUNMUS Gaming Headset Xbox',
+    'image': '/images/headset-rumnus.jpg',
+    'description':
+      'RUNMUS K2 gaming headset delivers 7.1 surround sound to enhance the immersive gaming experience. With the high responsive audio drivers, you can be better to make out the direction from which the sound is coming, like the gun fire, enemy footsteps and scenario indicators. Let you gain the competitive edge you need to take your game to the next level.',
+    'brand': 'RUNMUS',
+    'category': 'Electronics',
+    'price': 13.91,
+    'countInStock': 5,
+    'rating': 4.4,
+    'numReviews': 86699,
+  },
+  {
+    '_id': '4',
+    'name': 'Logitech G502 HERO',
+    'image': '/images/mouse-logitech.jpg',
+    'description':
+      'Logitech updated its iconic G502 gaming mouse to deliver even higher performance and more precise functionality than ever. Logitech G502 Hero high Performance Gaming mouse features the next generation Hero 16K Optical sensor, The highest performing and most efficient gaming sensor Logitech has ever made. An all-New lens and an updated tracking algorithm deliver ultra-precise tracking with no acceleration, smoothing, or filtering over the entire DPI range. Now, customize RGB mouse lighting to match your style and environment or sync to other Logitech G products. No matter your gaming style, It\'s easy to tweak G502 Hero to match your requirements, with custom profiles for your games, adjustable sensitivity from 200 up to 16, 000 DPI, and a tunable weight system that allows for tuning and balancing of up to five 3. 6G weights for just the right balance and feel.',
+    'brand': 'Logitech G',
+    'category': 'Electronics',
+    'price': 42.75,
+    'countInStock': 11,
+    'rating': 4.7,
+    'numReviews': 20411,
+  },
+  {
+    '_id': '5',
+    'name': 'Oculus Quest 2',
+    'image': '/images/vr-oculus-quest-2.jpg',
+    'description':
+      'Oculus Quest 2 is our most advanced all-in-one VR system yet. Every detail has been engineered to make virtual worlds adapt to your movements, letting you explore awe-inspiring games and experiences with unparalleled freedom. No PC or console required. Get the most out of each moment with blazing-fast performance and next-generation graphics. Stay focused with a stunning display that features 50% more pixels than the original Quest. Or take a break from the action and grab front-row seats to live concerts, exclusive events and more. The redesigned Touch controllers feature improved ergonomics and intuitive controls that transport your gestures, motions and actions directly into VR. You can even connect your VR headset to a gaming-compatible computer with an Oculus Link cable to access hundreds of PC VR games and experiences. Quest 2 also lets you bring your friends into the action. With live casting, you can share your VR experience with people around you. Or meet up with friends in virtual worlds to battle in multiplayer competitions or just spend some time together. With Oculus Quest 2, there’s no end in sight to what you can play, create and discover in virtual reality.',
+    'brand': 'Oculus',
+    'category': 'Electronics',
+    'price': 438.30,
+    'countInStock': 7,
+    'rating': 4.8,
+    'numReviews': 51196,
+  },
+  {
+    '_id': '6',
+    'name': 'GameSir T4 pro Wireless Game Controller',
+    'image': '/images/alexa.jpg',
+    'description':
+      'GameSir T4 pro\'s chip has been upgraded in all aspects. This chip can now meet the hopes of players using GameSir T4 pro on multiple platforms. It only takes a few simple steps to use this controller on Apple, Android, Android TV Box, Mac OS, PC, and Switch.',
+    'brand': 'GameSir',
+    'category': 'Electronics',
+    'price': 59.99,
+    'countInStock': 0,
+    'rating': 4.3,
+    'numReviews': 4040,
+  },
+]
+
+
+export default products

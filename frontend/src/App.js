9d29e5929ac318e10b6ccff2e8cefb451d5dface
mostafa-asefy <asefi.v@gmail.com>
2021-07-12 13:41:43 +0430

    lession 8: Product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen';
 
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
         <Container className='py-3'>
-          <h1>This is the main part</h1>
+          <HomeScreen />
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }

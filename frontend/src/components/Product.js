a2f9658a149557db42ac9bb066eff03577addcb8
mostafa-asefy <asefi.v@gmail.com>
2021-07-13 17:04:43 +0430

    lession 10: Rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,25 +1,26 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 function Product({ product }) {
     return (
         <div>
             <Card className="p-3 my-3 rounded">
                 <a href={`/product/${product._id}`}>
-                    <Card.Img src={product.image} />
+                    <Card.Img src={product.image} className="card-img-top" />
                 </a>
                 <Card.Body>
-                    <a href={`/product/${product._id}`}>
-                        <Card.Title as="h4">
+                    <a href={`/product/${product._id}`} >
+                        <Card.Title as="h5">
                             <strong>{product.name}</strong>
                         </Card.Title>
                     </a>
 
                     <Card.Text as="div" className="my-3">
-                        {product.rating} from {product.numReviews} reviews
+                        <Rating value={product.rating} text={`${product.numReviews} reviews`} color={"#F8E825"} />
                     </Card.Text>
 
-                    <Card.Text as="h5">
+                    <Card.Text as="h3">
                         ${product.price}
                     </Card.Text>
                 </Card.Body>

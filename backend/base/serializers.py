b793e49b905ebe9114dbfa729699e5a2314c5951
mostafa-asefy <asefi.v@gmail.com>
2021-08-26 11:38:07 +0430

    Ch02-L16-Serializers

---
+++ backend/base/serializers.py
@@ -0,0 +1,9 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .products import products
+from .models import Product
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProductSerializer
 
 routes = [
     '/api/v1/products/',
@@ -27,14 +28,13 @@ def getRoutes(request):
 
 @api_view(http_method_names=['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(http_method_names=['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)

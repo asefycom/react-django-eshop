37cfc24d505c53e1125e271550efaad6dc0bff44
mostafa-asefy <asefi.v@gmail.com>
2021-08-26 09:12:50 +0430

    Ch02-L15-again-Images

--- backend/base/models.py
+++ backend/base/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True)
-    # image =
+    image = models.ImageField(null=True, blank=True)
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)

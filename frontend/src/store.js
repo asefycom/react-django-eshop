5dad46878c9430855fef37916a64fba20336a256
mostafa-asefy <asefi.v@gmail.com>
2021-09-21 17:23:58 +0430

    Ch03-L04-Product Reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { listProductReducer } from './reducers/productReducers'
 
-const reducer = combineReducers({})
+const reducer = combineReducers({
+    listProduct: listProductReducer,
+})
 const initialState = {}
 const middleware = [thunk]
 

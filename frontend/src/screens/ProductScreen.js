2f999b5f47b1ab5e4cc69af8aa034422c42f923e
mostafa-asefy <asefi.v@gmail.com>
2021-10-04 11:39:06 +0330

    Ch04-Qty Item

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import { fetchProductDetails } from '../actions/productActions'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 
 function ProductScreen({ match }) {
+    const [qty, setQty] = useState(1)
     const dispatch = useDispatch()
     const productDetails = useSelector(state => state.productDetails)
     const { error, loading, product } = productDetails
 
     useEffect(() => {
         dispatch(fetchProductDetails(match.params.id))
-
     }, [dispatch, match])
 
 
@@ -69,6 +69,25 @@ function ProductScreen({ match }) {
                                                 </Col>
                                             </Row>
                                         </ListGroup.Item>
+                                        {product.countInStock > 0 && (
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Qty:</Col>
+                                                    <Col>
+                                                        <Form.Control as="select" custom value={qty} onChange={(e) => setQty(e.target.value)}>
+                                                            {
+                                                                [...Array(product.countInStock).keys()].map((x) => (
+                                                                    <option key={x + 1} value={x + 1}>
+                                                                        {x + 1}
+                                                                    </option>
+                                                                ))
+                                                            }
+                                                        </Form.Control>
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+                                        )}
+
                                         <ListGroup.Item>
                                             <Button type="button" className="btn-block" disabled={product.countInStock === 0}>Add to Card</Button>
                                         </ListGroup.Item>

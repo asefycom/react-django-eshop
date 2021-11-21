6e5ac7fa1134d592f7b9b1aee1e949ace9551bb7
mostafa-asefy <asefi.v@gmail.com>
2021-11-21 09:33:28 +0330

    Ch04-L03-AddToCart Reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { listProductReducer, detailsProductReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
 
 const reducer = combineReducers({
     listProduct: listProductReducer,
     productDetails: detailsProductReducer,
+    cart: cartReducer,
 })
 const initialState = {}
 const middleware = [thunk]

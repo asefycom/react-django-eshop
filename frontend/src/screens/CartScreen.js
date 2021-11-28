9b68cd62aeedb3586846e73332f98dd93e891684
mostafa-asefy <asefi.v@gmail.com>
2021-11-28 20:57:05 +0330

    Ch04-L06-Cart View - Items

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
 import { addToCart } from '../actions/cartActions'
 import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col, ListGroup, Image, Form } from 'react-bootstrap'
+import Message from '../components/Message'
+import { Link } from 'react-router-dom'
 
 function CartScreen({ match, location }) {
     const productId = match.params.id
@@ -9,7 +12,6 @@ function CartScreen({ match, location }) {
 
     const cart = useSelector(state => state.cart)
     const { cartItems } = cart
-    console.log(cartItems)
 
     useEffect(() => {
         if (productId) {
@@ -20,9 +22,55 @@ function CartScreen({ match, location }) {
 
 
     return (
-        <div>
-            Cart
-        </div>
+        <Row>
+            <Col md={8}>
+                {cartItems.length === 0 ? (
+                    <Message variant='info' text='Your cart is empty!' />
+                ) : (
+                    <ListGroup variant='flush'>
+                        {cartItems.map(item => (
+                            <ListGroup.Item key={item.product}>
+                                <Row>
+                                    <Col md={2}>
+                                        <Image src={item.image} alt={item.name} fluid rounded />
+                                    </Col>
+
+                                    <Col md={3}>
+                                        <Link to={`/product/${item.product}`}> {item.name}</Link>
+                                    </Col>
+
+                                    <Col md={2}>
+                                        ${item.price}
+                                    </Col>
+
+                                    <Col md={3}>
+                                        <Form.Control
+                                            as="select"
+                                            value={item.qty}
+                                            onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
+                                            {
+                                                [...Array(item.countInStock).keys()].map((x) => (
+                                                    <option key={x + 1} value={x + 1}>
+                                                        {x + 1}
+                                                    </option>
+                                                ))
+                                            }
+                                        </Form.Control>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                )}
+            </Col>
+
+
+
+            <Col md={4}>
+
+            </Col>
+
+        </Row>
     )
 }
 
